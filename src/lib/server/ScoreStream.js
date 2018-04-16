8ca9e26c8811f40bb351d03b22b9712dd7a88043
Michael Clayton <mclayton@redhat.com>
2018-04-16 12:59:51 -0400

    attempt tolerating miroservice B outages

--- src/lib/server/ScoreStream.js
+++ src/lib/server/ScoreStream.js
@@ -5,7 +5,18 @@ const KEEPALIVE_INTERVAL = 5 * 1000;
 
 class ScoreStream extends ReconnectingWebSocket {
   constructor(url, keepalive = false) {
-    super(url, undefined, { constructor: Html5Websocket });
+    super(url, undefined, {
+      constructor: Html5Websocket,
+      maxReconnectionDelay: 10000,
+      minReconnectionDelay: 1500,
+      reconnectionDelayGrowFactor: 1.3,
+      connectionTimeout: 60 * 60 * 1000,
+      maxRetries: Infinity,
+      debug: false
+    });
+
+    this.onerror = err =>
+      console.log(`[ScoreStream] Error: ${err.code}, ${err}`);
 
     this.addEventListener("open", () => {
       console.log("[ScoreStream] connected");
